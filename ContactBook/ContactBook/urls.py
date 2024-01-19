56cd4c359be92bea5d6fc0e7ba8b8b3bab04bb12
Min2-Kumar <mkreborn2@gmail.com>
2024-01-19 23:04:23 +0530

    update, delete, messeage

--- ContactBook/ContactBook/urls.py
+++ ContactBook/ContactBook/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.create_retrive, name='addshow'),
     path('update/<int:id>/',views.update_data, name='update'),
+    path('delete/<int:id>/', views.delete_data, name="delete"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ContactBook/app/views.py
+++ ContactBook/app/views.py
@@ -1,19 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from .models import Directory
 from .forms import DirectoryForm
-
+from django.contrib import messages
 
 def create_retrive(request):  # create and retrive
-    print('hi')
     if request.method == 'POST':
         form = DirectoryForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            messages.info(request, "your contact detail added successfully !!")
     
     form = DirectoryForm()
-
     person = Directory.objects.all()
-    return render(request, 'app/add.html',{'form':form, 'person':person})
+    return render(request, 'app/add.html',{'form':form,'person':person})
 
 # -----------------------------------------------------------------------------
 
@@ -23,8 +22,17 @@ def update_data(request, id): # update
         form = DirectoryForm(request.POST, request.FILES, instance=pid)
         if form.is_valid():
             form.save()
+            messages.success(request, "your contact detail updated successfully !!")
     
     pid = Directory.objects.get(pk=id)
     form = DirectoryForm(instance=pid)
 
-    # return render(request, 'app/index.html',{'form':form})
+    return render(request, 'app/updatedata.html',{'form':form})
+
+# -----------------------------------------------------------------------------------------
+
+def delete_data(request,id):    # delete
+    if request.method == 'POST':
+        pid = Directory.objects.get(pk=id)
+        pid.delete()
+        return HttpResponseRedirect('/')
